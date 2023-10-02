996425396f9bfae3b166ee2f9679e709339e08d2
Crivello Anthony <crivelloa>
2023-10-02 18:10:37 -0400

    Program Created

---
+++ src/comp1050_q05/Comp1050_q05.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package comp1050_q05;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Comp1050_q05 {
+
+	/**
+	 * 
+	 */
+	public Comp1050_q05() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your favorite day of the week: ");
+		String s = input.nextLine();
+		System.out.printf("Your favorite day of the week is %s.%n",s);
+	}
+
+}
